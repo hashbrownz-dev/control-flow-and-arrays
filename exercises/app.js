00f47dbcda2f2bd95f0f62e21320555339f391e5
Roosevelt Blow <rooseveltblow@gmail.com>
2022-10-12 15:22:35 -0600

    adds solutions for exercise 4 and exercise 5

--- exercises/app.js
+++ exercises/app.js
@@ -23,3 +23,33 @@ if(age >= 18){
 } else {
     console.log("Access Denied");
 }
+
+//Exercise 4
+let num2 = 35;
+let output = "";
+if(num2 > 0){
+    output = "The number is positive";
+    if(num2 >= 100){
+        output += " and greater than or equal to 100.";
+    } else {
+        output += " and less than 100."
+    }
+} else {
+    output = "The number is negative.";
+}
+console.log(output);
+
+//Exercise 5
+let grade = 95;
+
+if(grade >= 90){
+    console.log("A");
+} else if (grade >= 80){
+    console.log("B");
+} else if (grade >= 70){
+    console.log("C");
+} else if (grade >= 55){
+    console.log("D")
+} else {
+    console.log("F");
+}
