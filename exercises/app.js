a4a1a910f98b48a5686d096a8124d2a6a38dba01
Roosevelt Blow <rooseveltblow@gmail.com>
2022-10-12 14:29:03 -0600

    Completes in class Exercise 1-3

--- exercises/app.js
+++ exercises/app.js
@@ -1,1 +1,18 @@
-console.log("What's up fam?");
+//Exercise 1
+
+let num = 6;
+
+if(!(num % 2)){
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+//Exercise 3
+
+let age = 12;
+if(age >= 18){
+    console.log("Access Granted");
+} else {
+    console.log("Access Denied");
+}
