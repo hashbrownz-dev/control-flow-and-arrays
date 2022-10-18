154ccd1612d714e0e797c973460229f2bc81d4e5
Roosevelt Blow <rooseveltblow@gmail.com>
2022-10-18 14:04:55 -0600

    use an array of objects

--- exercises/objectsAndOrcs/app.js
+++ exercises/objectsAndOrcs/app.js
@@ -2,9 +2,16 @@ const adventurer = {
     name: 'Timothy',
     hitpoints: 10,
     belongings: ['sword','potion','Tums'],
-    companion: {    //Use an object within an object
+    companion: {            //Use an object within an object
         name: "Velma",
-        type: "Bat"
+        type: "Bat",
+        companion: {        //Use an object within an object
+            name: "Tina",
+            type: "Parasite",
+            belongings: [   //Use an array within an object within an object
+                'SCUBA tank', 'health insurance'
+            ]
+        }
     }
 }
 
@@ -20,3 +27,22 @@ for ( let i = 0; i < adventurer.belongings.length; i++ ) {
 console.log(adventurer.companion);
 //Access the companion's name
 console.log(adventurer.companion.name);
+//Access the companion's type
+console.log(adventurer.companion.type);
+//Access the companion's companion's type
+console.log(adventurer.companion.companion.type);
+//Access the companion's companion's belongings
+console.log(adventurer.companion.companion.belongings[1]);
+
+//Use an array of objects
+const movies = [
+    {title: "Tokyo Story"},
+    {title: "Paul Blart: Mall Cop"},
+    {title: "L'Avventura"}
+]
+//Reference using index
+console.log(movies[0].title);
+//Loop over the array and print all of the titles
+for (let i = 0; i < movies.length; i++){
+    console.log(movies[i].title);
+}
