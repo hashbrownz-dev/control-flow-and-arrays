69291d9b099f68cb9e78e1c788eb2583855b9f5f
Roosevelt Blow <rooseveltblow@gmail.com>
2022-10-18 13:47:27 -0600

    use an object within an object

--- exercises/objectsAndOrcs/app.js
+++ exercises/objectsAndOrcs/app.js
@@ -1,1 +1,22 @@
-console.log('ello guv');
+const adventurer = {
+    name: 'Timothy',
+    hitpoints: 10,
+    belongings: ['sword','potion','Tums'],
+    companion: {    //Use an object within an object
+        name: "Velma",
+        type: "Bat"
+    }
+}
+
+console.log(adventurer.belongings);
+//console.log(adventurer.belongings[0]);
+
+//Iterate over an array that is within an object
+for ( let i = 0; i < adventurer.belongings.length; i++ ) {
+    console.log(adventurer.belongings[i]);
+}
+
+//Access the companion object
+console.log(adventurer.companion);
+//Access the companion's name
+console.log(adventurer.companion.name);
